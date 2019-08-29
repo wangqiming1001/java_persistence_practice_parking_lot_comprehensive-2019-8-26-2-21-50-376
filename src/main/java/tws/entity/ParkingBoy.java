8ae2e29c2eb26b8ba765cec857b6e06759ee4c3f
wangqiming1001 <512821390@qq.com>
2019-08-29 22:01:27 +0800

    entity

---
+++ src/main/java/tws/entity/ParkingBoy.java
@@ -0,0 +1,17 @@
+package tws.entity;
+
+public class ParkingBoy {
+
+	private String parkingBoyID;
+
+	public String parkingBoyID() {
+		return parkingBoyID;
+	}
+
+	public void setEmployeeID(String parkingBoyID) {
+		this.parkingBoyID = parkingBoyID;
+	}
+	
+	
+	
+}
---
+++ src/main/java/tws/entity/ParkingLot.java
@@ -0,0 +1,22 @@
+package tws.entity;
+
+public class ParkingLot {
+	
+	private String parkingLotID;
+	private int capacity;
+	public String getParkingLotID() {
+		return parkingLotID;
+	}
+	public void setParkingLotID(String parkingLotID) {
+		this.parkingLotID = parkingLotID;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	
+	
+}
